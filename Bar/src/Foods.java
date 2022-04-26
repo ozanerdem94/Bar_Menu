2f477649cfdc05bc1695c85b6dba7ab53630a449
ozanerdem94 <104253005+ozanerdem94@users.noreply.github.com>
2022-04-26 20:24:30 +0200

    Create Foods.java

---
+++ Bar/src/Foods.java
@@ -0,0 +1,27 @@
+
+public class Foods extends WriteMenu{
+
+	String name;
+	double preis;
+	boolean available;
+	
+	public Foods(String name, double preis, boolean available) {
+		super(name, preis, available);
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+//	public void printMenu() {
+//		if(available==true) {
+//		System.out.format("%s,  $%f ", name, preis,available);
+//		System.out.println();
+//		}
+//		else {
+//			System.out.format("%s, $%f,%b ", name,preis,available);
+//			System.out.println("Sorry it is not available");
+//		}
+		
+	}
+	
+
+
