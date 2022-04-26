d53991704a125825a42a663b4cb4777942fb47c7
ozanerdem94 <104253005+ozanerdem94@users.noreply.github.com>
2022-04-26 20:24:05 +0200

    Create Desserts.java

---
+++ Bar/src/Desserts.java
@@ -0,0 +1,18 @@
+
+public class Desserts extends WriteMenu{
+
+	
+	String name;
+	double preis;
+	boolean available;
+	
+	
+	public Desserts(String name,double preis, boolean available) {
+		super(name,preis, available);
+		// TODO Auto-generated constructor stub
+	}
+	
+
+	
+	
+}
