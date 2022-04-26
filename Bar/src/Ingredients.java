26da4f08504b22d0ab407649bfb112623e67a792
ozanerdem94 <104253005+ozanerdem94@users.noreply.github.com>
2022-04-26 20:24:33 +0200

    Create Ingredients.java

---
+++ Bar/src/Ingredients.java
@@ -0,0 +1,40 @@
+
+public class Ingredients {
+
+	public void Beer() {
+		System.out.println("Ingredients are: ");
+		System.out.println("water, starch source, hops");
+	}
+
+	public void Vodka() {
+		System.out.println("Ingredients are: ");
+		System.out.println("water, ethanol");
+	}
+
+	public void AppleJuice() {
+		System.out.println("Ingredients are: ");
+		System.out.println("water, ascorbic acid, sugar, apple concentrate");
+	}
+
+	public void Whiskey() {
+		System.out.println("Ingredients are: ");
+		System.out.println("water, barley, yeast");
+	}
+
+	public void OrangeJuice() {
+		System.out.println("Ingredients are: ");
+		System.out.println("water, ascorbic acid, sugar, orange concentrate");
+	}
+
+	public void Lemonade() {
+		System.out.println("Ingredients are: ");
+		System.out.println("water, ascorbic acid, sugar, lemon concentrate");
+	}
+
+	public void Wine() {
+		System.out.println("Ingredients are: ");
+		System.out.println("water, ethanol,grape juice concentrate, flavours");
+	}
+
+	
+}
